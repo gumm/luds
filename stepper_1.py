ac995d20cea7fd0a740de557900c25cadf081af3
Jan Badenhorst <jan@trintel.co.za>
2017-08-10 21:29:48 +0200

    Stepper motor class

--- stepper_1.py
+++ stepper_1.py
@@ -8,67 +8,71 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# CONSTANTS -----------------------------
-
-# These are the pins corresponding to the
-# stepper motor inpits.
-CON_PINS = [14, 15, 18, 23]
-
-# The step sequince
+# The step sequence
 SEQ = [
-    [1,0,0,0],
-    [1,1,0,0],
-    [0,1,0,0],
-    [0,1,1,0],
-    [0,0,1,0],
-    [0,0,1,1],
-    [0,0,0,1],
-    [1,0,0,1],
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1],
     ]
 
-class SM_28BJY48:
-    
-    # We want to keep track of the step count
-    SC = 0
 
-    # METHODS -----------------------
-    def reset():
+class StepperMotor28BJY48:
+    def __init__(self, con_pins=None):
+
+        # These are the pins corresponding to the
+        # stepper motor inputs.
+        self.CON_PINS = con_pins if con_pins else [14, 15, 18, 23]
+
+        # We want to keep track of the step count
+        self.SC = 0
+
+        # Set the pins to outputs, and make them all low
+        for pin in self.CON_PINS:
+            GPIO.setup(pin, GPIO.OUT)
+        self.reset()
+
+    def reset(self):
         """
         Set all pins low
         """
-        for pin in CON_PINS:
-            GPIO.output(pin, 0)
+        for p in self.CON_PINS:
+            GPIO.output(p, 0)
         
-    def turn(sc, ang=360, dir=0, steps=None):
+    def turn(self, ang=360, direction=0, steps=None):
         """
         Execute stepping.
         Accepts either a number of steps, or an
         angle, and a direction.
         When given both an angle and steps, the
-        angle parametr is ignored.
+        angle parameter is ignored.
         When given neither steps nor an angle, one full
         rotation is executed.
         Default direction is counter-clock-wise.
-        Acceps 'cw' and 'ccw' as accseptable dir values.
+        Accepts 'cw' and 'ccw' as acceptable dir values.
         Else 0 or False is counter-clock-wise or
-        True or anything that evalues to true is
+        True or anything that evaluates to true is
         considered clock-wise
         """
         
         # Parse the string values for dir
-        if dir == 'cw':
-            dir = 1
-        if dir == 'ccw':
-            dir = 0
+        if direction == 'cw':
+            direction = 1
+        if direction == 'ccw':
+            direction = 0
         
         # Prepare to either count up or down
         # depending on the direction given
-        counter = 1 if dir else -1
+        counter = 1 if direction else -1
         
         # If either the steps or the angle is zero
         # there is no point in doing anything.
         if steps == 0 or ang == 0:
-            return sc
+            return
         
         # When given steps, use it as is, else
         # convert the given angle to the nearest
@@ -78,26 +82,25 @@ class SM_28BJY48:
         # Depending on the given direction, rotation is either
         # a list from 0 to 7 (ccw) or 7 to 0 (cw)
         rotation = list(range(8))
-        rotation = list(reversed(rotation)) if dir else rotation
+        rotation = list(reversed(rotation)) if direction else rotation
         
         # Depending on the given direction, the pins are traversed either
         # from 0 to 3 (ccw) or 3 to 0 (cw)
         pins = list(range(4))
-        pins = list(reversed(pins)) if dir else pins
+        pins = list(reversed(pins)) if direction else pins
         
-        # Loop through the sequnce the required number of steps.
+        # Loop through the sequence the required number of steps.
         # setting the GPIO output to either 1 or 0 as defined by
-        # the sequnce.
+        # the sequence.
         for i in range(steps):
-            sc += counter
+            self.SC += counter
             for step in rotation:
-                for pin in pins:
-                    GPIO.output(CON_PINS[pin], SEQ[step][pin])
+                for p in pins:
+                    GPIO.output(self.CON_PINS[p], SEQ[step][p])
                 sleep(0.001)
-        reset()
-        return sc
+        self.reset()
 
-    def go_zero(v):
+    def go_zero(self):
         """
         Return the number of steps required to get back
         to 0 if 1 rotation is 512 steps.
@@ -105,23 +108,14 @@ class SM_28BJY48:
         Half rotation is 256, and this is as far as
         it is possible to get away from 0
         """
-        s = v % 512
+        s = self.SC % 512
         steps = 256 - (s - 256) if s > 256 else s
-        return turn(SC, steps=steps, dir=s > 256)
+        return self.turn(steps=steps, direction=s > 256)
             
 
-
-for pin in CON_PINS:
-    GPIO.setup(pin, GPIO.OUT)
-reset()
-
-
-
-SC = turn(SC, dir='cw')
-print(SC)
-
+motor = StepperMotor28BJY48()
+motor.turn(ang=45)
 sleep(2)
-print('-------------------------- RESET NOW -----')
-SC = go_zero(SC)
-print(SC)
+motor.go_zero()
+
 GPIO.cleanup()
