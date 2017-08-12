75c6d65bc8525b8b062d45e17f998a94cba09a6b
Jan Badenhorst <jan@trintel.co.za>
2017-08-12 15:22:29 +0200

    Dev

--- adc/simpletest.py
+++ adc/simpletest.py
@@ -69,7 +69,7 @@ def reading_to_degrees(pin, v):
     high = cal['high']
     span = high - low
     throw = cal['throw']
-    return v - low
+    return ((v - low) / span) * throw
 
 if args.calibrate and args.pin is not None:
     # Main loop.
