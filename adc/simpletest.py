4d69fc81a14d07997b24e96ce298695ce009a92f
Jan Badenhorst <jan@trintel.co.za>
2017-08-12 13:59:05 +0200

    Dev

--- adc/simpletest.py
+++ adc/simpletest.py
@@ -34,11 +34,15 @@ LOW = 1
 HIGH = 26359
 RANGE = HIGH - LOW
 
+ARR = [0, 1, 2, 3]
+RES = []
+
 # Main loop.
 while True:
 
     # Read all the ADC channel values in a list.
     # Read the specified ADC channel using the previously set gain value.
-    v = round(adc.read_adc(0, gain=GAIN) / RANGE * 270)
-    print(v)
+    for p in ARR:
+        RES[p] = adc.read_adc(p, gain=GAIN)
+    print(RES)
     time.sleep(0.1)
