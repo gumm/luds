34c1462a3fffb427548f8612d163dd6a8e05df7d
Jan Badenhorst <jan@trintel.co.za>
2017-08-13 09:41:58 +0200

    Dev

--- adc/simpletest.py
+++
@@ -1,9 +0,0 @@
-# Simple demo of reading each analog input from the ADS1x15 and printing it to
-# the screen.
-# Author: Tony DiCola
-# License: Public Domain
-import time
-from adc.ADS1115 import ADC_ADS1115
-
-test = ADC_ADS1115()
-test.run()
---
+++ main_adc.py
@@ -0,0 +1,4 @@
+from adc.ADS1115 import ADC_ADS1115
+
+test = ADC_ADS1115()
+test.run()
