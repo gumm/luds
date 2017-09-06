0e1838f37d69c9e6cbf229284cc9388062fa50e3
Jan Badenhorst <jan@trintel.co.za>
2017-09-06 23:20:43 +0200

    Pipes rather than the master queue

--- main_motor_2.py
+++ main_motor_2.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     for i in range(5):
         for d in DD:
             knie.put(['goto', d[1], 0.01])
-            enkel.put(['goto', d[0], 0.1])
+            enkel.put(['goto', d[0], 0.01])
             print(knie_recv.recv())
             print(enkel_recv.recv())
 
