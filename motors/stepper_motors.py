42df62aa78f7d92533168d1c6c776e6d996e05ee
Jan Badenhorst <jan@trintel.co.za>
2017-09-07 14:14:31 +0200

    Calc target

--- motors/stepper_motors.py
+++ motors/stepper_motors.py
@@ -186,7 +186,7 @@ class Sm28BJY48:
         calc_target = self.POS - sign * steps * self.DPS
 
         self.POS = target_pos
-        print('%s: op:%s, p:%s delta:%s sign:%s steps:%s calc_target:%s POS:%s' % (
+        print('%s: curr:%s, target:%s delta:%s sign:%s steps:%s calc_target:%s POS:%s' % (
             self.name, op, target_pos, delta, sign, steps, calc_target, self.POS))
 
     def go_to_pos_1(self, p, duration=None):
